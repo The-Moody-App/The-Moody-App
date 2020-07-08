6ba49fd4477b955a8bd56ca6e8d6a150f78d0b04
SaraAghaAlnimer <SaraAghaAlnimer@gmail.com>
2020-07-08 11:50:18 +0300

    rewriting api configuration to print out single playlist data

--- server/spotify-playlist.js
+++ server/spotify-playlist.js
@@ -1,5 +1,5 @@
 var  request = require('request');
-var  user_id = "224nlnmrxjp6thocol5zoriui";
+var  user_id = "trevorana";
 var token = "BQDdM9jqiN_AJhQkpoupvrTrLKdMLNOCn9GwghGIQdxG6_R3dC3E0W8u8rna1KKlEewjPEeh8oTALPNlxEZViezOCZzE2b_OyJRBco7Yzgf2yP3BhuEct4FnlIW9jmHkTZpLF7TADEqZgM9CpvXX6F0rmBoj6w57Yg";
 var playlists_url = "https://api.spotify.com/v1/users/"+ user_id+"/playlists"
 
@@ -8,20 +8,21 @@ request({url: playlists_url, headers:{Authorization:token}}, function(err, res) 
         console.log("error getting playlist!")
     }
     if (res){
-        console.log(res);
+        
         var  playlists = JSON.parse(res.body);
-        console.log(JSON.stringify(playlists.items, null, " "));
-        var playlist_url = ""; //playlists[0].herf
+        // console.log(JSON.stringify(playlists.items, null, " "));
+        var playlist_url =  playlists.items[0].href
         request({url: playlist_url, headers:{Authorization:token}}, function(err, res) {
-            if (err) {
-                console.log("error getting playlist!")
-            }
+           
             if (res){
                 var  playlist = JSON.parse(res.body);
                 console.log("playlist: " + playlist.name);
                 playlist.tracks.items.forEach(function(track) {
                     console.log(track.track.name);
                 });
+            }
+            if (err) {
+                console.log("error getting playlist!")
             }
         });    
                 
