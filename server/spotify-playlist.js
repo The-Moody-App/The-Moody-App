36c8988bbd1df1cc09f4ae143708d0875974f4f5
SaraAghaAlnimer <SaraAghaAlnimer@gmail.com>
2020-07-08 08:12:14 +0300

    conflict resolved, api access token is added

--- server/spotify-playlist.js
+++ server/spotify-playlist.js
@@ -1,15 +1,17 @@
 var  request = require('request');
 var  user_id = "224nlnmrxjp6thocol5zoriui";
-var token = "BQAPjM2sGCF9vfZtS-Ly7F940q-vZl1rKOVlFMgRxE57bLHHtbhUfBCOTXVywULiqmeJhKqnskM_4frpMVH0bFAeRewL1i34vYBV27A6Gbcf6kPhHxRYt-_HyH1HA790ob8NP08WediDS3OgE18KeetoKFxlVgZkEg"
-var playlist_url = "https://api.spotify.com/v1/users/"+user_id+"/playlists"
+var token = "BQDdM9jqiN_AJhQkpoupvrTrLKdMLNOCn9GwghGIQdxG6_R3dC3E0W8u8rna1KKlEewjPEeh8oTALPNlxEZViezOCZzE2b_OyJRBco7Yzgf2yP3BhuEct4FnlIW9jmHkTZpLF7TADEqZgM9CpvXX6F0rmBoj6w57Yg";
+var playlists_url = "https://api.spotify.com/v1/users/"+ user_id+"/playlists"
 
-request({url: playlist_url, headers:{Authorization:token}}, function(err, res) {
+request({url: playlists_url, headers:{Authorization:token}}, function(err, res) {
     if (err) {
         console.log("error getting playlist!")
     }
     if (res){
+        console.log(res);
         var  playlists = JSON.parse(res.body);
-        var playlist_url = "";//playlists[0].herf:
+        console.log(JSON.stringify(playlists.items, null, " "));
+        var playlist_url = ""; //playlists[0].herf
         request({url: playlist_url, headers:{Authorization:token}}, function(err, res) {
             if (err) {
                 console.log("error getting playlist!")
@@ -17,8 +19,8 @@ request({url: playlist_url, headers:{Authorization:token}}, function(err, res) {
             if (res){
                 var  playlist = JSON.parse(res.body);
                 console.log("playlist: " + playlist.name);
-                playlist.tracks.forEach(function(track) {
-                    console.log(tracks.track.name);
+                playlist.tracks.items.forEach(function(track) {
+                    console.log(track.track.name);
                 });
             }
         });    
