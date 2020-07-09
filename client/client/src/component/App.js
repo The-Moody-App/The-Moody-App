93579a2dae0fb61bd41cd0b8c7bf249a8599eb2b
SaraAghaAlnimer <SaraAghaAlnimer@gmail.com>
2020-07-09 08:23:13 +0300

    copied client files from master brach only. added app get and post requests to generate user token and redirect to spotify account. set env variables: client id and client secret id. deployed backend server to heroku

---
+++ client/client/src/component/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import "./style.css";
+import Nav from "./nav";
+import login from "./login";
+import logout from "./logOut";
+
+
+import Signup from "./signup";
+import Body from "./home";
+import About from "./aboutus";
+import Profile from "./profile";
+// import Contact from "./contactus";
+import Footer from "./footer";
+import Commuinty from "./commuinty";
+import Mood from "./mood";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Route path="/" exact component={Body} />
+          <Route path="/login" component={login} />
+          <Route path="/logOut" component={logout} />
+
+          <Route path="/signup" component={Signup} />
+          <Route path="/profile" component={Profile} />
+          <Footer />
+          {/* <Route path="/contactus" component={Contact} /> */}
+          <Route path="/aboutus" component={About} />
+          <Route path="/commuinty" component={Commuinty} />
+          <Route path="/mood" component={Mood} />
+          {/* <Route path="/" exact component={Body} />
+          <Route path="/" exact component={Body} /> */}
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
 const express = require("express");
+const request = require('request')
+const querystring = require('querystring')
 var cors = require('cors')
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose")
 
-
 const app = express();
-
 
 //make connection between server side and client side
 app.use(cors());
-
 
 //routs path
 const users = require("./routes/user");
@@ -36,10 +35,48 @@ app.use(bodyParser.json());
 
 //use routs for the site
 app.use("",users);
-app.use("",songs)
 
 
-var port = 5000;
+var redirect_uri = 
+  process.env.REDIRECT_URI || 
+  'http://localhost:5000/callback'
+
+app.get('/login', function(req, res) {
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      scope: 'user-read-private user-read-email',
+      redirect_uri
+    }))
+})
+
+app.get('/callback', function(req, res) {
+  let code = req.query.code || null
+  let authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(
+        process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+      ).toString('base64'))
+    },
+    json: true
+  }
+  request.post(authOptions, function(error, response, body) {
+    var access_token = body.access_token
+    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+    res.redirect(uri + '?access_token=' + access_token)
+  })
+})
+
+
+
+var port = process.env.PORT || 5000;
 
 app.listen(port, function () {
   console.log(' listening on port ' + port);
